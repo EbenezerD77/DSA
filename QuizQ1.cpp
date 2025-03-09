87b4043037041e783861907f7d689f981790154f
Ebenezer D. <107511952+12ebenezer@users.noreply.github.com>
2025-03-09 18:53:34 +0300

    Added .cpp files for C++ quiz.

---
+++ QuizQ1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+// QUIZ 1 QUESTION 1
+
+int main(){
+    int arr[15] = {0};
+    arr[0] = 10;
+    arr[14] = 150;
+
+    // test 
+    cout << arr[0] << endl;
+    cout << arr[8] << endl;
+    cout << arr[14] << endl;
+
+    return 0;
+}
